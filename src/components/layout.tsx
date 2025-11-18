b78c7758401305f967a9206cf50687af36134f10
johnny-mathias <johnnydiasmathiasjunior@gmail.com>
2025-11-17 23:01:00 -0300

    feat: White and Dark themes created

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,26 +1,39 @@
 import { Link, Outlet } from "react-router-dom";
+import { useTheme } from "../context/theme-context";
+import { ThemedText } from "../types/themed-text";
 
 export function Layout() {
-    return (
-        <div>
-            <nav className="flex flex-row items-center bg-purple-950 border-b-2 border-purple-900 text-white px-4 h-14">
-                <div className="ml-16 flex flex-1 gap-4">
-                    <Link to="/home">
-                        <span className="font-bold bg-linear-to-r from-[#5170ff] to-[#ff66c4] text-transparent bg-clip-text">
-                            Connectia
-                        </span> {"<>"} Home</Link>
-                    <Link to={"/integrantes"}>Integrantes</Link>
-                    <Link to={"/sobre"}>Sobre</Link>
-                    <Link to={"/contato"}>Contato</Link>
+    const { theme, toggleTheme } = useTheme();
+    const { gradientText } = ThemedText();
 
+    return (
+        <div className="bg-(--bg) text-(--text) min-h-screen transition-colors">
+
+            <nav className="flex flex-row items-center bg-(--bg-secondary) border-(--border) border-b text-(--text) px-4 h-14">
+                <div className="ml-16 flex flex-1 gap-4">
+                    <Link
+                        className="hover:underline"
+                        to="/">
+                        <span
+                            className={`font-bold ${gradientText}`}>
+                            Connectia
+                        </span>
+                        {"<>"} Home</Link>
+                    <Link className="hover:underline" to={"/integrantes"}>Integrantes</Link>
+                    <Link className="hover:underline" to={"/sobre"}>Sobre</Link>
+                    <Link className="hover:underline" to={"/contato"}>Contato</Link>
                 </div>
 
-                <div>
+                <div className="flex items-center gap-4">
+                    <button onClick={toggleTheme} className="hover:opacity-70 transition">
+                        {theme === "light" ? "🌙 Escuro" : "☀️ Claro"}
+                    </button>
+
                     <button className="hover:cursor-pointer">Conta</button>
                 </div>
             </nav>
 
             <Outlet />
         </div>
-    )
+    );
 }
