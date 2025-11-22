61d9d0f18a04b39565477dd8c25ba4448b08716c
johnny-mathias <johnnydiasmathiasjunior@gmail.com>
2025-11-22 01:20:50 -0300

    style: members card visual improvements

--- src/components/aluno.tsx
+++ src/components/aluno.tsx
@@ -4,21 +4,23 @@ import { FaLinkedin } from "react-icons/fa";
 
 export function Aluno({ nome, img, rm, github, linkedin }: AlunoProps) {
     return (
-        <div className="mt-6 bg-(--bg-2) rounded-xl p-2 text-(--text) mb-4 px-12 py-12 shadow-xl">
-            <h4 className="text-(--title) font-bold mb-2">{nome}</h4>
+        <div className="flex flex-col justify-center items-center bg-(--bg-2) text-center rounded-xl p-2 text-(--text) mb-4 px-4 py-8 w-90 h-105 shadow-xl">
+            <h4 className="text-(--title) text-xl font-bold mb-2">{nome}</h4>
             <div>
                 <img src={img} alt={`Integrante ${nome}`}
-                    className="block mx-auto m-1 mb-4 w-32 h-32
+                    className="block mx-auto m-1 mb-4 w-40 h-40 
                                 rounded-full border-3 border-solid object-cover text-(--border-2)" />
-                <p>1TDSPV</p>
-                <p>RM: {rm}</p>
+                <div className="text-lg">
+                    <p>1TDSPV</p>
+                    <p>RM: {rm}</p>
+                </div>
             </div>
-            <div className="flex gap-4 mt-2 place-content-center">
+            <div className="flex gap-4 mt-8 place-content-center">
                 <a href={github} target="_blank" rel="noopener noreferrer">
-                    <FaGithub size={24} />
+                    <FaGithub size={32} />
                 </a>
                 <a href={linkedin} target="_blank" rel="noopener noreferrer">
-                    <FaLinkedin size={24} />
+                    <FaLinkedin size={32} />
                 </a>
             </div>
         </div>
