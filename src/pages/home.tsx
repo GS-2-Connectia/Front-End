838eebd2a69bdfea970ccc2ecee0f4387f4ecc73
johnny-mathias <johnnydiasmathiasjunior@gmail.com>
2025-11-23 18:18:25 -0300

    fix: Home const problem fixed

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,13 +3,13 @@ import { CourseList } from "../components/course-list";
 import { CoursesContext } from "../context/course-context";
 
 export function Home() {
-    const { courses } = useContext(CoursesContext);
+    const { Courses } = useContext(CoursesContext);
 
     return (
         <div>
             <main>
                 <h1 className="title">Home</h1>
-                <CourseList courseList={courses} />
+                <CourseList courseList={Courses} />
             </main>
         </div>
     )
