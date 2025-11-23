13054fc2efaeb1144f20ab1ebbb002c436291f04
johnny-mathias <johnnydiasmathiasjunior@gmail.com>
2025-11-23 18:13:22 -0300

    feat: footer added

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <div className="bg-(--bg-2) min-h-1 py-2 text-center">
+            <p>Connectia © Todos os Direitos Reservados.</p>
+        </div>
+    )
+}
